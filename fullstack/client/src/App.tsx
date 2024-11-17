d36f6c2f8b1a38603ed122f00b42aa043631ca5a
Stellla <114156332+khk9562@users.noreply.github.com>
2024-11-17 21:04:50 +0900

    feat: set environment for react+ts+vite+tailwind css

---
+++ fullstack/client/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+    <h1 className="text-3xl font-bold text-gray-800">
+      Vite + React + Tailwind CSS
+    </h1>
+  </div>
+  )
+}
+
+export default App
