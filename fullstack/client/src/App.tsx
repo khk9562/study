80f31b6b74ebd970dda2726f2101bd5057a19599
stella <khk9562@naver.com>
2024-12-15 17:51:19 +0900

    fix: server environment

--- fullstack/client/src/App.tsx
+++ fullstack/client/src/App.tsx
@@ -1,14 +1,13 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
-    <h1 className="text-3xl font-bold text-gray-800">
-      Vite + React + Tailwind CSS
-    </h1>
-  </div>
-  )
+      <h1 className="text-3xl font-bold text-gray-800">
+        Vite + React + Tailwind CSS + Typescript
+      </h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- fullstack/server/app/config/database.py
+++ fullstack/server/app/config/database.py
@@ -7,11 +7,12 @@ import os
 load_dotenv()
 
 # MySQL 연결 설정
-MYSQL_USER = os.getenv("MYSQL_USER", "root")
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "your_password")
-MYSQL_HOST = os.getenv("MYSQL_HOST", "127.0.0.1")
-MYSQL_PORT = os.getenv("MYSQL_PORT", "3306")
-MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "STARTDB")
+MYSQL_USER = os.getenv("DB_USERNAME", "root")
+MYSQL_PASSWORD = os.getenv("DB_PASSWORD", "your_password")
+# MYSQL_HOST = os.getenv("DB_HOST", "127.0.0.1")
+MYSQL_HOST = os.getenv("DB_HOST", "localhost")
+MYSQL_PORT = os.getenv("DB_PORT", "3306")
+MYSQL_DATABASE = os.getenv("DB_NAME", "STARTDB")
 
 # MySQL URL 생성
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
